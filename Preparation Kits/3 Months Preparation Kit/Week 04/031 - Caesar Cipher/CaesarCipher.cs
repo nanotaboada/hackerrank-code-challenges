dcab843427d002c2675537a8d05695bd5aa679da
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2024-01-18 12:10:00 -0300

    chore: remove function placeholder

--- Preparation Kits/3 Months Preparation Kit/Week 04/031 - Caesar Cipher/CaesarCipher.cs
+++ Preparation Kits/3 Months Preparation Kit/Week 04/031 - Caesar Cipher/CaesarCipher.cs
@@ -1,15 +1,5 @@
 class Result
 {
-
-    /*
-     * Complete the 'caesarCipher' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. STRING s
-     *  2. INTEGER k
-     */
-
     // https://en.wikipedia.org/wiki/Caesar_cipher
     
     public static string caesarCipher(string s, int k)
